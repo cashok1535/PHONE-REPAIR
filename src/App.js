8936c24a921c65cdd6ef78975537f9d52ae7e664
cashok1535 <cashok1928@gmail.com>
2025-07-16 13:11:09 -0700

    add cursor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import { useCallback, useEffect, useState } from "react";
 import { Header } from "./components/Header";
 
 function App() {
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const handleMouseMove = useCallback((e) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  }, []);
+  useEffect(() => {
+    document.addEventListener("mousemove", handleMouseMove);
+    return ()=>{
+      document.removeEventListener("mousemove", handleMouseMove)
+    }
+  }, [handleMouseMove]);
   return (
     <div className="App">
+      <div
+        className="cursor"
+        style={{
+          position: "absolute",
+          top: cursorPosition.y,
+          left: cursorPosition.x,
+        }}
+      ></div>
       <Header />
     </div>
   );
