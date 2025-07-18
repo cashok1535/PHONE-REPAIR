08f0903df32e26b77c4ba9921216fba4e4aa6d86
cashok1535 <cashok1928@gmail.com>
2025-07-17 17:43:08 -0700

    complete interactive phone

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import { Cursor } from "./components/Cursor";
 import { Header } from "./components/Header";
 import { Main } from "./components/Main";
 
 function App() {
   return (
     <div className="App">
-      <Cursor />
+      {" "}
       <Header />
       <Main />
     </div>
--- src/components/Phone.js
+++ src/components/Phone.js
@@ -5,6 +5,7 @@ export const Phone = () => {
   const [isMouseEnter, setIsMouseEnter] = useState(false);
   const [positionOnPhone, setPositionOnPhone] = useState({ x: 0, y: 0 });
   const phoneRef = useRef(null);
+  console.log(positionOnPhone);
 
   const handleMouseLeave = useCallback(() => {
     setIsMouseEnter(false);
@@ -12,14 +13,14 @@ export const Phone = () => {
   const handleMouseEnter = useCallback(() => {
     setIsMouseEnter(true);
   }, []);
-  const handleMouseMove = useCallback(
-    (e) => {
-      const { left, top } = phoneRef.current.getBoundingClientRect();
-      setPositionOnPhone({ x: e.clientX - left, y: e.clientY - top });
-      console.log(positionOnPhone);
-    },
-    [positionOnPhone]
-  );
+  const handleMouseMove = useCallback((e) => {
+    const { left, top, height, width } =
+      phoneRef.current.getBoundingClientRect();
+    setPositionOnPhone({
+      x: e.clientX - left - width / 2,
+      y: e.clientY - top - height / 2,
+    });
+  }, []);
   useEffect(() => {
     const phone = phoneRef.current;
 
@@ -40,8 +41,20 @@ export const Phone = () => {
     }
   }, [handleMouseMove, isMouseEnter]);
   return (
-    <div className="interactive__flex__interactive">
-      <img ref={phoneRef} className="phone" src={phoneImg} alt="" />
+    <div ref={phoneRef} className="interactive__flex__interactive">
+      <img
+        style={{
+          transform: isMouseEnter
+            ? `rotateY(${positionOnPhone.x / 10}deg) rotateX(${
+                positionOnPhone.y / 10
+              }deg)`
+            : "rotateY(0deg) rotateX(0deg)",
+          transition: "transform .2s",
+        }}
+        className="phone"
+        src={phoneImg}
+        alt=""
+      />
     </div>
   );
 };
