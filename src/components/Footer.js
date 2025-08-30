eb2e3c8bb896576032a596002b2fac2b1c4eb392
cashok1535 <cashok1928@gmail.com>
2025-08-30 19:13:50 +0300

    complete main page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,22 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <ContactForm />
+      <div className="footer__line">
+        <svg
+          style={{ width: "100%" }}
+          xmlns="http://www.w3.org/2000/svg"
+          height="31"
+        >
+          <path
+            d="M0 15.5 L3000 15.5"
+            style={{
+              fill: "none",
+              stroke: "rgba(255, 255, 255, 0.16)",
+              strokeWidth: "1px",
+            }}
+          ></path>
+        </svg>
+      </div>
       <FooterLinks />
       <div className="footer__line">
         <svg
@@ -21,6 +37,10 @@ export const Footer = () => {
             }}
           ></path>
         </svg>
+      </div>
+      <div className="footer__bottom">
+        <div className="footer__bottom__left">© Created by</div>
+        <div className="footer__bottom__right">All rights Reserved</div>
       </div>
     </footer>
   );
