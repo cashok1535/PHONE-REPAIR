7d2c445d6f674b1c0af2530018f760676fbda6ce
cashok1535 <cashok1928@gmail.com>
2025-10-03 12:31:04 +0300

    add about us page procces

--- src/components/AboutUsPage.js
+++ src/components/AboutUsPage.js
@@ -1,58 +1,11 @@
-import imgFirst from "../img/AboutUsPageFirst.webp";
-import imgSecond from "../img/AboutUsPageSecond.webp";
+import { AboutUsPageSection } from "./AboutUsSectionOnPage";
+import { AboutUsPageProcess } from "./AboutUsPageProcess";
 
 export const AboutUsPage = () => {
   return (
     <section className="about__us__page">
-      <div className="about__us__page__info">
-        <div className="interactive__flex__info__title about__us__page__center">
-          About Us<span className="interactive__flex__info__orange">.</span>
-        </div>
-        <div className="page__info__little">
-          <div className="page__home">HOME</div>
-          <span className="page__home__arrow">→</span>
-          <div className="page__current__page">ABOUT US</div>
-        </div>
-      </div>
-      <div className="about__us__page__flex">
-        <div className="about__us__page__flex__element">
-          <div className="orange__title">ABOUT US</div>
-          <div className="what__we__do__title about__us__page__flex__title">
-            We Fix Gadgets<span class="interactive__flex__info__orange">.</span>
-          </div>
-          <div className="about__us__section__text__bold about__us__page__flex__little__title">
-            Trust TechPro – Where Tech Meets Pro!
-          </div>
-          <div className="about__us__section__text">
-            At TechPro, we are dedicated to being your go-to destination for all
-            things mobile repair in New York. With years of expertise under our
-            belt, our team of skilled technicians excels in diagnosing and
-            resolving a wide array of issues plaguing your devices.
-          </div>
-          <img className="about__us__page__img" src={imgFirst} alt="" />
-          <div className="about__us__section__text">
-            We understand the frustration that comes with a malfunctioning
-            device, which is why we strive to provide swift and reliable
-            solutions that get you back to enjoying your gadgets in no time.
-          </div>
-        </div>
-        <div className="about__us__page__flex__element">
-          <img className="about__us__page__img" src={imgSecond} alt="" />
-          <div className="about__us__section__text">
-            We're not just another mobile repair service – we're your trusted
-            partners in keeping your devices running smoothly. With over 25
-            years of experience serving the New York community, our mission is
-            simple: to provide top-quality repairs and exceptional customer
-            service.
-            <br />
-            <br />
-            Our team of 15 dedicated technicians brings a wealth of expertise to
-            every repair job, ensuring that your devices are in capable hands.
-            From cracked screens to water damage, battery issues to software
-            glitches, we've seen it all and fixed it all.
-          </div>
-        </div>
-      </div>
+      <AboutUsPageSection />
+      <AboutUsPageProcess />
     </section>
   );
 };
---
+++ src/components/AboutUsPageProcess.js
@@ -0,0 +1,52 @@
+export const AboutUsPageProcess = () => {
+  return (
+    <section className="about__us__page__process">
+      <div className="about__us__page__process__title">
+        <div className="orange__title">PROCESS</div>
+        <div className="what__we__do__title">
+          Revive, Restore, Reconnect
+          <span className="interactive__flex__info__orange">.</span>
+        </div>
+        <div className="about__us__section__text__bold about__us__page__process__text">
+          All you need to do is to trust tech pros.
+        </div>
+      </div>
+      <div className="about__us__page__process__flex">
+        <div className="about__us__page__process__flex__element">
+          <div className="interactive__flex__info__orange about__us__page__orange">
+            01.
+          </div>
+          <div className="what__we__do__grid__element__title">Contact Us</div>
+          <div className="about__us__section__text about__us__page__text">
+            Reach out to TechPro via phone, email, or our website to set an
+            appointment in one of our offices.
+          </div>
+        </div>
+        <div className="about__us__page__process__flex__element">
+          <div className="interactive__flex__info__orange about__us__page__orange">
+            02.
+          </div>
+          <div className="what__we__do__grid__element__title">
+            Diagnostic Evaluation
+          </div>
+          <div className="about__us__section__text about__us__page__text">
+            Visit our conveniently located repair centers in NY and bring your
+            smartphone along for diagnostics.
+          </div>
+        </div>
+        <div className="about__us__page__process__flex__element">
+          <div className="interactive__flex__info__orange about__us__page__orange">
+            03.
+          </div>
+          <div className="what__we__do__grid__element__title">
+            Expert Repair
+          </div>
+          <div className="about__us__section__text about__us__page__text">
+            Once the repair is complete, we'll notify you promptly. Return to
+            our center to pick up your smartphone.
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
